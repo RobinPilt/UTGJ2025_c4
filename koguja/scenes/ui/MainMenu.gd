7df387331d8e8b9c1e66a471863257a0af3cf2ae
arts05 <aklets123@gmail.com>
2025-10-05 01:29:06 +0300

    Added sound to button press, needs tweaking

--- koguja/scenes/ui/MainMenu.gd
+++ koguja/scenes/ui/MainMenu.gd
@@ -4,9 +4,17 @@ extends Control
 @onready var start_btn: Button = $"CenterContainer/VBoxContainer/StartButton"
 @onready var quit_btn: Button = $"CenterContainer/VBoxContainer/QuitButton"
 @onready var title_lbl: Label = $"CenterContainer/VBoxContainer/TitleLabel"
+@onready var sound = preload("res://assets/audio/ui_select.wav")
+
+func _on_button_pressed():
+	print("Pressed")
+	$AudioStreamPlayer.stream = sound
+	$AudioStreamPlayer.play()
 
 func _ready() -> void:
 	# Connect button signals
+	start_btn.pressed.connect(_on_button_pressed)
+	quit_btn.pressed.connect(_on_button_pressed)
 	start_btn.pressed.connect(_on_start_pressed)
 	quit_btn.pressed.connect(_on_quit_pressed)
 
